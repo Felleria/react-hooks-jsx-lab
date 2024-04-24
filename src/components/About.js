64a1359cb0437416558de5c5b6824c89f6e82196
Felleria <tellytabbie@gmail.com>
2024-04-24 12:19:02 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+   <div>
+    <h2>About Me</h2>
+    <p>Lorem ipsum</p>
+    <img src={image} alt="i made this"></img>
+   </div>
+  );
 }
 
 export default About;
